953b14d51a4eebe9d7be9ce1b54c991a4d380a6f
Jesus Soto <sotous0@gmail.com>
2025-06-21 10:20:49 -0500

    Use env variables

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
   }
 });
 
