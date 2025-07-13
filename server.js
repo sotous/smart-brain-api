21f4ccb84d9e422a31746e4b3b2de6c73d09a996
Jesus Soto <sotous0@gmail.com>
2025-07-13 12:14:06 -0500

    Require the auth middleware for protected routes

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
+const auth = require('./controllers/authorization');
 const db = knex({ 
   // connect to your own database here:
   client: 'pg',
@@ -20,11 +21,12 @@ app.use(express.json()); // latest version of exressJS now comes with Body-Parse
 app.use(morgan('combined'));
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
