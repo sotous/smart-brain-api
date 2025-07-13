33f867f0eac3f62532cd40771e86be17ceecfdca
Jesus Soto <sotous0@gmail.com>
2025-07-13 12:10:06 -0500

    Add the user schema

---
+++ postgres/tables/user.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email text NOT NULL UNIQUE,
+    entries BIGINT DEFAULT 0,
+    joined TIMESTAMP NOT NULL
+);
+
+COMMIT;
