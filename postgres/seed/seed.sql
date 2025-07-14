f54fa8d089c57f4559ac785a7fc9e9f7d5ef750c
Jesus Soto <sotous0@gmail.com>
2025-07-14 06:27:09 -0500

    Correct double quotes issue

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (name, email, pet, age, entries, joined) VALUES ('John Doe', 'john@doe.com', 52, "dog", 10, '2021-01-01');
+INSERT INTO users (name, email, age, pet, entries, joined) VALUES ('John Doe', 'john@doe.com', 52, 'dog', 10, '2021-01-01');
 INSERT INTO login (hash, email) VALUES ('$2a$10$Iwvi.Lbc4OxggfGrLMLROO8Uf15wu1nNcBLMHtgRBjf9/xhKT2z0S', 'john@doe.com');
 
 COMMIT;
