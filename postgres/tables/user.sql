26d0d0cba0e46d3c4dd4816f1c1469e393773144
Jesus Soto <sotous0@gmail.com>
2025-07-25 07:02:36 -0500

    Add user profile pic to schema

--- postgres/tables/user.sql
+++ postgres/tables/user.sql
@@ -3,9 +3,10 @@ BEGIN TRANSACTION;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email text NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
     pet VARCHAR(50),
     age INT,
+    profile_pic TEXT,
     entries BIGINT DEFAULT 0,
     joined TIMESTAMP NOT NULL
 );
