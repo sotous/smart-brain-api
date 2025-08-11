b5d774e36652799166299920684929a7353c5151
Jesus Soto <sotous0@gmail.com>
2025-08-11 09:45:48 -0500

    Include profilePic on profile update

--- controllers/profile.js
+++ controllers/profile.js
@@ -13,10 +13,10 @@ const handleProfileGet = (req, res, db) => {
 
 const handleProfileUpdate = (req, res, db) => {
   const { id } = req.params;
-  const { name, age, pet } = req.body.formInput;
+  const { name, age, pet, profilePic } = req.body.formInput;
   db('users')
     .where({ id })
-    .update({ name, age, pet })
+    .update({ name, age, pet, profile_pic: profilePic })
     .then(resp => {
       if (resp) {
         res.json('success')
